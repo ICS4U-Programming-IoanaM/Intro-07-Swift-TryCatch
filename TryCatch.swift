54884615832988beb160b04fd4181105c15717f8
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-02-23 14:50:52 +0000

    completion of Intro-07 assignment (calculates volume of a sphere with try catch)

--- TryCatch.swift
+++ TryCatch.swift
@@ -9,9 +9,37 @@ import Foundation
 //
 //  A program that calculates the volume of a sphere, with try catch.
 
-print("Enter the radius (cm): ")
-let radius = Double(readLine()!)!
+// creates custom errors
+enum InputError: Error {
+  // invalid input error
+  case invalidInput
+}
 
-let volume = (4 / 3) * Double.pi * pow(radius, 3)
+// function to get user input that returns a double
+func userInput() throws -> Double {
+  // 
+  print("Enter the radius (cm): ")
+  guard let input = readLine(), let radius = Double(input) else {
+    throw InputError.invalidInput
+  }
+  return radius
+}
 
-print("The volume of the sphere is \(round(volume * 1000)/1000.0) cm^3")
+do {
+  // calls function to get user input and 'tries' it
+  let radius = try userInput()
+
+  // volume calculation based on user input
+  let volume = (4.0 / 3.0) * Double.pi * pow(radius, 3)
+
+  // output to console the volume of the sphere.
+  print("The volume of the sphere is \(round(volume * 1000)/1000.0) cm^3")
+
+  // if user imputed invalid input
+} catch InputError.invalidInput {
+  print("Invalid input, please enter a number.")
+
+  // in all honesty I don't know what this does but it's needed
+} catch {
+  print("An unexpected error occurred.")
+}
